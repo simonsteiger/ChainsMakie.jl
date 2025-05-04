887ae937484e1e5897769a9eb1f11b3bbbd9f494
simonsteiger <simon.j.steiger@gmail.com>
2025-05-04 09:29:10 +0200

    add ref tests for density functions

--- src/density.jl
+++ src/density.jl
@@ -1,3 +1,28 @@
+@recipe(ChainsDensity) do scene
+    Attributes(
+        color = nothing, # TODO define a safe way to map custom colors -- maybe take `colormap` instead?
+    )
+end
+
+function Makie.plot!(md::ChainsDensity{<:Tuple{<:AbstractMatrix}})
+    mat = md[1]
+    for chain in eachcol(mat[])
+        density!(md, chain)
+    end
+    return md
+end
+
+function Makie.density(chains::Chains, parameters; hidey=true, kwargs...)
+    fig = Figure()
+    for (i, parameter) in enumerate(parameters)
+        ax = Axis(fig[i, 1])
+        hidex = i < length(parameters)
+        _axisdecorations!(ax, hidex, "Parameter estimate", hidey, parameter)
+        chainsdensity!(chains[:, parameter, :]; kwargs...)
+    end
+    return fig
+end
+
 function _axisdecorations!(ax, hidex, xlabel, hidey, ylabel)
     if hidey
         ax.ylabel = ylabel
@@ -9,41 +34,4 @@ function _axisdecorations!(ax, hidex, xlabel, hidey, ylabel)
         ax.xlabel = xlabel
     end
     return ax
-end
-
-function Makie.density!(ax::Axis, mat::AbstractMatrix; kwargs...)
-    for i in axes(mat, 2)
-        density!(ax, mat[:, i]; kwargs...)
-    end
-    return nothing
-end
-
-function Makie.density!(mat::AbstractMatrix; kwargs...)
-    ax = current_axis()
-    density!(ax, mat; kwargs...)
-    return nothing
-end
-
-function Makie.density(chains::Chains, parameters; hidey=true, kwargs...)
-    fig = Figure()
-    for (i, parameter) in enumerate(parameters)
-        ax = Axis(fig[i, 1])
-        hidex = i < length(parameters)
-        _axisdecorations!(ax, hidex, "Parameter estimate", hidey, parameter)
-        density!(chains[:, parameter, :]; kwargs...)
-    end
-    return fig
-end
-
-function Makie.density!(axs::Vector{Axis}, chains::Chains, parameters; hidey=true)
-    if length(axs) != length(parameters)
-        error(DimensionMismatch("`axs` and `parameters` must be the same length"))
-    end
-    for (i, (ax, parameter)) in enumerate(zip(axs, parameters))
-        hidex = i < length(parameters)
-        _axisdecorations!(ax, hidex, "Parameter estimate", hidey, parameter)
-        # TODO access the color cycler here and add alpha to fill
-        density!(ax, chains[:, parameter, :]; strokearound = true)
-    end
-    return nothing
 end
